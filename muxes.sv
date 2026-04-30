449340ea4e5a0b619c94b345d88ee9b00bfc591b
jamiexshih <jamieshih@ucsb.edu>
2026-04-30 11:11:49 -0700

    Update muxes.sv

--- muxes.sv
+++ muxes.sv
@@ -10,10 +10,10 @@ module muxes (
     // y1_o: 4-input mux
     always_comb begin
         case (sel4_i)
-            2'b00: y1_o = d_i;
+            2'b00: y1_o = 1'b0;
             2'b01: y1_o = c_i;
             2'b10: y1_o = 1'b0;
-            2'b11: y1_o = 1'b0;
+            2'b11: y1_o = d_i;
             default: y1_o = 1'b0;
         endcase
     end
